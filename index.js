3bc4eb6c6f21a68f8ad4ee28c5111ccc590f9af3
Anastasia19081999 <anastasia121598@gmail.com>
2023-09-08 17:51:07 +0300

    Home work 1.2.3

---
+++ index.js
@@ -0,0 +1,38 @@
+// Отримати кільксть товарі що купив користувач і ціну товара та вивести суму за всі товари
+
+
+
+let quantityOfGoods = prompt('Скільки у вас товару?');
+let priceOfTheProduct = 5;
+
+alert(quantityOfGoods * priceOfTheProduct);
+
+
+
+
+
+
+// отримати ціну покупки від користувача ( через prompt) та вивесту ціну товара зі знижкою   10%. 
+
+// let purchasePrice = prompt('Вкажіть ціну покупки');
+// let tallage = 10;
+
+// let result = purchasePrice / 100 * tallage;
+// alert(result);
+
+
+
+
+
+
+//Отримати 4 значення від користувача і розрахувати середнє арефметичне значення. Результат вивести в консоль чи alert
+
+
+// let a = Number(prompt("Напишіть число"));
+// let b = Number(prompt("Напишіть число"));
+// let c = Number(prompt("Напишіть число"));
+// let d = Number(prompt("Напишіть число"));
+
+// let result = a + b + c + d;
+// let coltresult = result / 4;
+// alert(coltresult);
